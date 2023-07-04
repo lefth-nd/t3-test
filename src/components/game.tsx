c45c9c29c949ee170db4aa4ae7f5a2a7f9d6b123
Jay <schu0527@flinders.edu.au>
2023-07-04 18:17:33 +0930

    non sln

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -3,20 +3,27 @@ import { api } from "~/utils/api";
 
 // TODO: need different way of getting random video id
 
-function getVID() {
+function GetVID() {
   const { data } = api.youtube.video.useQuery();
+  const [video_index_state, setVideoIndex] = useState(0);
+
+  const incrementVideoIndex = () => {
+    setVideoIndex(video_index_state + 1);
+  };
+
   if (data?.items !== undefined) {
-    const v_id = data.items[4]?.id as string;
-    const snippet = data.items[4]?.snippet;
+    const v_id = data.items[video_index_state]?.id as string;
+    const snippet = data.items[video_index_state]?.snippet;
     const title = snippet?.title as string;
-    return [v_id, title];
+    console.log(video_index_state);
+    return [v_id, title, incrementVideoIndex];
   } else {
     return [];
   }
 }
 
 function getComment() {
-  const videoData = getVID();
+  const videoData = GetVID();
   const video_id = videoData[0] as string;
   const title = videoData[1];
   console.log(title);
@@ -34,28 +41,36 @@ function getComment() {
 }
 
 export const Game = () => {
-  const btn =
-    "rounded-full bg-white px-10 py-3 font-semibold text-gray-800 no-underline transition hover:bg-red-900 hover:text-white";
+  const button =
+    "hidden rounded-full bg-white px-10 ml-4 py-3 font-semibold text-gray-800 no-underline transition hover:bg-red-900 hover:text-white ";
   const heading =
     "hidden text-2xl font-extrabold tracking-tight text-white sm:text-[2rem]";
+
   const commentDetails = getComment();
   const comment = commentDetails[0] as string[];
-  const answer = commentDetails[1];
+  const answer = commentDetails[1] as string;
 
   const [title, setHeadingVisible] = useState(heading);
+  const [button_state, setButtonVisible] = useState(button);
 
   let textarea: HTMLTextAreaElement | null = null;
-
   if (typeof window !== "undefined") {
     textarea = document.getElementById("answer-title") as HTMLTextAreaElement;
   }
 
+  const handleClick = () => {
+    setHeadingVisible(heading);
+    setButtonVisible(button);
+  };
+
   textarea?.addEventListener("keydown", (event: KeyboardEvent) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
-      const visible_heading =
-        "text-2xl font-extrabold tracking-tight text-white sm:text-[2rem]";
+      const visible_heading = heading.substring(6);
+      const visible_button = button.substring(6);
       setHeadingVisible(visible_heading);
+      setButtonVisible(visible_button);
+      //set
     }
   });
 
@@ -63,7 +78,7 @@ export const Game = () => {
     <div className="w-3/4 text-center">
       {comment.map((index) => (
         <div
-          key={""}
+          key={index}
           className="h-32items-center mb-6 flex rounded-3xl bg-slate-200 p-8 align-middle font-bold shadow-lg shadow-zinc-950"
         >
           {index}
@@ -72,12 +87,17 @@ export const Game = () => {
       <div className="text-center">
         <h1 className={title}>{answer}</h1>
         <div className="pb-6"></div>
-        <textarea
-          id="answer-title"
-          title="user_input"
-          placeholder=""
-          className="w-3/4 resize-none items-center rounded-lg  border-2 bg-transparent pt-6 text-center text-xl  font-medium text-white"
-        ></textarea>{" "}
+        <div className="flex flex-row  justify-center gap-10">
+          <textarea
+            id="answer-title"
+            title="user_input"
+            placeholder=""
+            className="relative w-3/6 resize-none items-center rounded-lg border-2 bg-transparent  pt-6 text-center text-xl font-medium text-white"
+          ></textarea>{" "}
+          <button type="submit" className={button_state} onClick={handleClick}>
+            New Game
+          </button>
+        </div>
       </div>
     </div>
   );
