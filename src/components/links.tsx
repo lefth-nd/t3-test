52bdab51026cfb94f6f2071c9ecad92b1c409935
Jay <schu0527@flinders.edu.au>
2023-07-05 16:23:12 +0930

    updated landing

---
+++ src/components/links.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
+export const Links = () => {
+  return (
+    <div className="text-left uppercase text-white">
+      <div className="grid scale-75 grid-flow-col items-center gap-4">
+        <div className="grid-flow-row-dense gap-2">
+          <Link href="/algorithms" className="hover:underline">
+            <Image
+              className="rounded-full  shadow-black grayscale filter hover:shadow-lg hover:filter-none"
+              alt="dsa-image"
+              src="/dsa-image.png"
+              width={320}
+              height={320}
+            ></Image>
+          </Link>
+        </div>
+        <div className=""></div>
+        <div className="grid-flow-row-dense  gap-2">
+          <Link href="/hidden" className="hover:underline">
+            <Image
+              className="rounded-2xl shadow-black grayscale filter hover:shadow-lg hover:filter-none"
+              alt="title-me-image"
+              src="/title-me-image.png"
+              width={220}
+              height={220}
+            ></Image>
+          </Link>
+        </div>
+        <div className="grid-flow-row-dense gap-2">
+          <Link
+            href="https://github.com/lefth-nd/gpt-parse"
+            className="hover:underline"
+          >
+            <Image
+              className="rounded-full shadow-black grayscale filter hover:shadow-lg hover:filter-none"
+              alt="gpt-parse-image"
+              src="/gptp.png"
+              width={220}
+              height={220}
+            ></Image>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Image from "next/image";
+import { Links } from "~/components/links";
 export default function MainMenu() {
   const main =
     "flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#15162c] to-[#000000]";
@@ -7,60 +7,23 @@ export default function MainMenu() {
     "text-5xl font-extrabold tracking-tight text-slate-100 sm:text-[5rem]";
   return (
     <main className={main}>
-      <div className="h-1/2 w-1/2 rounded-md  border-2 border-indigo-900  bg-stone-950 py-10 ">
-        <div className="mr-10 flex flex-row items-center text-center">
+      <div className="h-1/2 w-1/2 rounded-md  border-b-2 border-stone-800   bg-stone-950 py-10">
+        <div className="flex flex-row items-center border-b-2 border-b-stone-800 px-10 text-center">
           <div className="ml-10 text-left">
             <h1 className={h1}>Welcome to the Testing Grounds</h1>
-            <h2 className="pt-10 text-[1.5rem] tracking-tight text-slate-300">
+            <h2 className="py-10 text-[1.5rem] tracking-tight text-slate-300">
               Experimental projects live here.
             </h2>
           </div>
-          <div className="p-8"></div>
-          <div className="mt-10  text-left uppercase text-white">
-            <div className="items-center gap-4">
-              <div className="grid-flow-row-dense gap-2">
-                <Link href="/algorithms" className="hover:underline">
-                  <Image
-                    className="rounded-full  shadow-black grayscale filter hover:shadow-lg hover:filter-none"
-                    alt="dsa-image"
-                    src="/dsa-image.png"
-                    width={320}
-                    height={320}
-                  ></Image>
-                </Link>
-                <div className="py-2"></div>
-              </div>
-              <div className=""></div>
-              <div className="grid-flow-row-dense  gap-2">
-                <Link href="/hidden" className="hover:underline">
-                  <Image
-                    className="rounded-md shadow-black grayscale filter hover:shadow-lg hover:filter-none"
-                    alt="title-me-image"
-                    src="/title-me-image.png"
-                    width={220}
-                    height={220}
-                  ></Image>
-                </Link>
-                <div className="py-2"></div>
-              </div>
-              <div className="grid-flow-row-dense gap-2">
-                <Link
-                  href="https://github.com/lefth-nd/gpt-parse"
-                  className="hover:underline"
-                >
-                  <Image
-                    className="rounded-full shadow-black grayscale filter hover:shadow-lg hover:filter-none"
-                    alt="gpt-parse-image"
-                    src="/gptp.png"
-                    width={220}
-                    height={220}
-                  ></Image>
-                </Link>
-                <div className="py-2"></div>
-              </div>
-            </div>
-          </div>
+          <Image
+            className="rounded-md shadow-black "
+            alt="avatar"
+            src="/cropped-avatar.png"
+            width={120}
+            height={120}
+          ></Image>
         </div>
+        <Links />
       </div>
       <div className="absolute bottom-2 text-slate-500">
         Created by: Jay (lefth-nd)
