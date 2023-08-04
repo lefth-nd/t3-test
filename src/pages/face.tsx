c4e3e16b27d376583aea7118d17502f5c0285ae1
Jay <schu0527@flinders.edu.au>
2023-08-04 15:41:00 +0930

    remove massive canvas package

--- src/pages/face.tsx
+++ src/pages/face.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { api } from "~/utils/api";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import Footer from "~/components/footer";
 
 const Face = () => {
   const [b64png, setPng] = useState("");
@@ -19,22 +20,25 @@ const Face = () => {
   }, [newb64png]);
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#15162c] to-[#000000] text-center">
-      <div className="text-[4rem] font-bold tracking-tight text-white">
-        Face Me!
-        <div className="m-10"></div>
-        <div className="">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#252525] to-[#000000] text-center">
+      <div className="rounded-lg  bg-lime-900 p-20">
+        <div className="text-[4rem] font-bold tracking-tight text-lime-100">
+          Face Me!
+          <div className="m-10 text-[1rem] font-normal tracking-wide ">
+            here&apos;s a cool new discord pfp for you...
+          </div>
           {b64png ? (
             <img
-              className="m-auto rounded-full shadow-lg"
+              className="m-auto rounded-full shadow-md shadow-slate-900"
               src={b64png}
               alt="Base64 PNG"
             />
           ) : (
-            <div className="">Loading...</div>
+            <div className="text-[2rem]">Loading...</div>
           )}
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
