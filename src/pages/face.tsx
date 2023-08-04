1170aee7d36c3dd16fd9bbbc0fdc262f961b5a98
Jay <schu0527@flinders.edu.au>
2023-08-04 15:51:30 +0930

    removed unused function

--- src/pages/face.tsx
+++ src/pages/face.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { api } from "~/utils/api";
-import Image from "next/image";
 import { useState, useEffect } from "react";
 import Footer from "~/components/footer";
 
--- src/server/api/routers/facegen.ts
+++ src/server/api/routers/facegen.ts
@@ -1,13 +1,5 @@
-import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import getFace from "~/server/face";
-
-function parseDOMDocument(value: unknown): Document {
-  if (value instanceof Document) {
-    return value;
-  }
-  throw new Error("invalid input");
-}
 
 export const FaceGenRoute = createTRPCRouter({
   generateFace: publicProcedure.query(({}) => {
